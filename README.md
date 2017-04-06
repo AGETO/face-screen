961be6d4cab1e62426fb7d1b22426465a8e9cd25
Andreas Mihm <andreas.mihm@diva-e.com>
2017-04-06 14:08:53 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# face-screen
+# diva-e - Face recognition
+Usage of [Microsoft Face API](https://westus.dev.cognitive.microsoft.com/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395236) 
+
+## Installation
+Make sure you have node installed. You need a keys.js file in the `static/`folder. It must contain 
+
+```
+    var FACE_API_KEY = '<Your Azure Face API key here>'
+    var FACE_LIST_ID = '<Your face list id>'
+```
+
+Install dependencies
+
+`npm install`
+
+Start the server:
+
+`node index.js`
+
+Open browser at `localhost:3000`
---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var path = require('path')
+var request = require('request')
+
+var app = express();
+
+app.use(express.static(path.join(__dirname,'static')))
+
+app.post('/customerinbar', function(req,res){
+	//This is your endpoint for pouring drinks
+	//If the customerid sent in exists in the orders, then drink is poured
+	//var newurl = 'http://104.47.146.9:8167/api/customer/customerinbar';
+	var newurl = 'http://localhost:8167/api/customer/customerinbar';
+	console.log(req)
+	/*
+	request({
+		url: newurl,
+		qs: req.query,
+		method: 'POST'
+	}).pipe(res)
+	*/
+})
+
+app.listen(3000);
+
+console.log('Listening on 3000')
